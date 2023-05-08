f59c88513c78231c9d1dd619c67afd1bfbd00983
Lukas Sandman <sandmanlukas@gmail.com>
2023-05-08 13:41:49 +0200

    Add support to add posts on homepage

--- src/challenges/cookies.js
+++ src/challenges/cookies.js
@@ -7,6 +7,28 @@ if (!localStorage.getItem("COOKIE")) {
 
 const db = await getDatabase("challenge1.sqlite");
 
+function getPosts(db) {
+  let posts = db.exec(`SELECT * FROM posts ORDER BY id DESC;`)[0]
+  .values;
+
+  const postTags = posts.map((post) => {
+    return `
+      <style>
+        .message {margin-bottom: 15px;}
+        .user {float:left; font-weight:bold; color:#009; margin-bottom: 5px; margin-right: 10px;}
+        .content { margin-left: 30px;margin-right: 10px;font-style:italic; color:#; }
+      </style>
+
+      <div class="message">
+        <div class="user">${post[1]}: </div>
+        <div class="content">${post[2]}</div>
+      </div>
+    `;
+  });
+
+  return postTags;
+}
+
 infoBtn(
   {
     title: "Cookie Monster",
@@ -21,8 +43,8 @@ infoBtn(
 try {
   let cookie = localStorage.getItem("COOKIE");
 
-  let stmt = `SELECT username FROM TrackedUsers where cookie_value = '${cookie}'`;
-  let usernameRes = db.exec(stmt); // Possible security flaw.
+  let cookieStmt = `SELECT username FROM TrackedUsers where cookie_value = '${cookie}'`;
+  let usernameRes = db.exec(cookieStmt);
   const username = usernameRes[0].values[0][0];
 
   // Conditional to show "Welcome back" screen
@@ -30,26 +52,51 @@ try {
     document.getElementById("welcome-back").style.visibility = "visible";
     document.getElementById("username-span").textContent = ` ${username}!`;
 
-    let posts = db.exec(`SELECT * FROM posts ORDER BY post_id DESC;`)[0]
-      .values;
+    // let posts = db.exec(`SELECT * FROM posts ORDER BY id DESC;`)[0]
+    //   .values;
 
-    const postTags = posts.map((post) => {
-      return `
-        <style>
-          .message {margin-bottom: 15px;}
-          .user {float:left; font-weight:bold; color:#009; margin-bottom: 5px; margin-right: 10px;}
-          .content { margin-left: 30px;margin-right: 10px;font-style:italic; color:#; }
-        </style>
+    // const postTags = posts.map((post) => {
+    //   return `
+    //     <style>
+    //       .message {margin-bottom: 15px;}
+    //       .user {float:left; font-weight:bold; color:#009; margin-bottom: 5px; margin-right: 10px;}
+    //       .content { margin-left: 30px;margin-right: 10px;font-style:italic; color:#; }
+    //     </style>
 
-        <div class="message">
-          <div class="user">${post[0]}: </div>
-          <div class="content">${post[1]}</div>
-        </div>
-      `;
+    //     <div class="message">
+    //       <div class="user">${post[1]}: </div>
+    //       <div class="content">${post[2]}</div>
+    //     </div>
+    //   `;
+    // });
+
+    let postTags = getPosts(db);
+    let forumPostsDiv = document.getElementById("forum-posts");
+    forumPostsDiv.innerHTML = postTags.join("");
+
+
+    const button = document.getElementById('button');
+
+    button.addEventListener('click', function (event) {
+      const text = document.getElementById("text-area").value;
+      
+      if (text){
+        let insertStmt =  db.prepare("INSERT INTO posts (user, content) VALUES (?, ?)");
+        insertStmt.bind([username, text]);
+        insertStmt.step();
+        insertStmt.free();
+        
+        postTags = getPosts(db);
+        forumPostsDiv.innerHTML = postTags.join("");
+        document.getElementById("text-area").value = "";
+
+      }
+
+
+
+
     });
 
-    const forumPostsDiv = document.getElementById("forum-posts");
-    forumPostsDiv.innerHTML = postTags.join("");
   }
 } catch (err) {
   document.getElementById("not-logged-in").style.visibility = "visible";
